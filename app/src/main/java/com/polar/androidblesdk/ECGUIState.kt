ba8a553eea86cf2aa03993c5a39fb1e8a9c2ade5
HanzFordy <haniffakhruddintanjunf@gmail.com>
2025-10-30 16:27:01 +0700

    fit: add MP Chart Entry import

--- app/src/main/java/com/polar/androidblesdk/ECGUIState.kt
+++ app/src/main/java/com/polar/androidblesdk/ECGUIState.kt
@@ -1,7 +1,8 @@
 package com.polar.androidblesdk
+import com.github.mikephil.charting.data.Entry
 
 sealed class ECGUIState {
     object Idle : ECGUIState()
-    data class Streaming(val message: String, val latency: Long? = null) : ECGUIState()
+    data class Streaming(val message: String, val latency: Long? = null, val ecgDataPoints: List<Entry>? = null) : ECGUIState()
     data class Error(val errorMessage: String) : ECGUIState()
 }
