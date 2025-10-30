f2ccd7cafcba4ec8e9fdec1c81ac55ac0c2a5d05
HanzFordy <haniffakhruddintanjunf@gmail.com>
2025-10-30 16:50:29 +0700

    fit: add new data class for classification markers, add val markers to data class Streaming

--- app/src/main/java/com/polar/androidblesdk/ECGUIState.kt
+++ app/src/main/java/com/polar/androidblesdk/ECGUIState.kt
@@ -3,6 +3,20 @@ import com.github.mikephil.charting.data.Entry
 
 sealed class ECGUIState {
     object Idle : ECGUIState()
-    data class Streaming(val message: String, val latency: Long? = null, val ecgDataPoints: List<Entry>? = null) : ECGUIState()
-    data class Error(val errorMessage: String) : ECGUIState()
+
+    data class Streaming(
+        val message: String,
+        val latency: Long? = null,
+        val ecgDataPoints: List<Entry>? = null,
+        val markers: List<ClassificationMarker>? = null
+    ) : ECGUIState()
+
+    data class ClassificationMarker(
+        val location: Entry, // Lokasi (x,y)
+        val lebel: String // Hasil klasifikasi
+    )
+
+    data class Error(
+        val errorMessage: String
+    ) : ECGUIState()
 }
